dcb8118711cd051c506a344faab4af0bbab88a7f
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-04-30 15:22:13 +0530

    minor changes

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 
 function FaCamera(props) {
-  return <svg style={{ cursor: 'pointer', position: 'absolute', bottom: '12px', right: '15px'}} stroke="currentColor" fill="dark-green" strokeWidth={0} viewBox="0 0 512 512" height="1.4em" width="1.4em" {...props}><path d="M512 144v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h88l12.3-32.9c7-18.7 24.9-31.1 44.9-31.1h125.5c20 0 37.9 12.4 44.9 31.1L376 96h88c26.5 0 48 21.5 48 48zM376 288c0-66.2-53.8-120-120-120s-120 53.8-120 120 53.8 120 120 120 120-53.8 120-120zm-32 0c0 48.5-39.5 88-88 88s-88-39.5-88-88 39.5-88 88-88 88 39.5 88 88z" /></svg>;
+  return <svg style={{ cursor: 'pointer', position: 'absolute', bottom: '0px', right: '4px'}} stroke="currentColor" fill="dark-green" strokeWidth={0} viewBox="0 0 512 512" height="1.4em" width="1.4em" {...props}><path d="M512 144v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h88l12.3-32.9c7-18.7 24.9-31.1 44.9-31.1h125.5c20 0 37.9 12.4 44.9 31.1L376 96h88c26.5 0 48 21.5 48 48zM376 288c0-66.2-53.8-120-120-120s-120 53.8-120 120 53.8 120 120 120 120-53.8 120-120zm-32 0c0 48.5-39.5 88-88 88s-88-39.5-88-88 39.5-88 88-88 88 39.5 88 88z" /></svg>;
 }
 
 
@@ -75,6 +75,7 @@ const RegisterForm = ({ styles }) => {
           {image.preview ? (
             <div style={{ position: "relative" }}>
               <Image
+                style={{ borderRadius: "50%"}}
                 src={image.preview}
                 alt="avatar"
                 width="150"
@@ -84,7 +85,13 @@ const RegisterForm = ({ styles }) => {
             </div>
           ) : (
             <div style={{ position: "relative" }}>
-              <Image src={Avatar} alt="avatar" width="150" height="150" />
+              <Image
+                style={{ borderRadius: "50%"}}
+                src={Avatar}
+                alt="avatar"
+                width="150"
+                height="150"
+              />
               <FaCamera onClick={uploadBtn} />
             </div>
           )}
